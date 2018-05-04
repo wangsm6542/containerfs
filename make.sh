d6df0cf4729b3cba064af839fcc9acf73c1db6c4
Jim Raynor <zhengxiaochuan-3@163.com>
2018-05-04 11:01:41 +0800

    Update make.sh

--- make.sh
+++ make.sh
@@ -18,7 +18,6 @@ cd ./cmd
 for dir in `ls` 
 do
   pushd $dir
-  go get
   go build -ldflags "-X github.com/tiglabs/containerfs/utils.Release=`git tag|head -n 1` -X github.com/tiglabs/containerfs/utils.Build=`git rev-parse HEAD`" -o cfs-$dir
   cp cfs-$dir  ../../build
   rm -rf cfs-$dir
